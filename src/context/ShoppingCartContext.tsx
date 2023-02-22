a81721e7a2bfef01b2dda0bd1f9c995c93b76cdc
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2023-02-22 23:28:51 +0530

    local storage used to save cart data to avoid data loss during refresh

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, ReactNode, useState } from "react";
 import { ShoppingCart } from "../components/ShoppingCart";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -29,7 +30,7 @@ export function useShoppingCart() {
 
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps){
-    const [cartItems, setCartItems] = useState<CartItem[]>([])
+    const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("shopping-cart",[])
     const [isOpen, setIsOpen] = useState(false)
 
     const openCart = () => setIsOpen(true);
